e5684b86e04834a34b886467887b4a1998879c14
JayashreeRavishankar <jayashree.sf3546@gmail.com>
2024-09-25 18:29:08 +0530

    Added sample

---
+++ MAUITreeView/MAUITreeView/Helper/TreeViewBehavior.cs
@@ -0,0 +1,36 @@
+﻿using Syncfusion.Maui.TreeView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUITreeView
+{
+    public class TreeViewBehavior : Behavior<SfTreeView>
+    {
+        protected override void OnAttachedTo(SfTreeView bindable)
+        {
+            bindable.NodeExpanded += Bindable_NodeExpanded;
+            bindable.NodeCollapsed += Bindable_NodeCollapsed;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(SfTreeView bindable)
+        {
+            bindable.NodeExpanded -= Bindable_NodeExpanded;
+            bindable.NodeCollapsed -= Bindable_NodeCollapsed;
+            base.OnDetachingFrom(bindable);
+        }
+
+        private void Bindable_NodeCollapsed(object? sender, NodeExpandedCollapsedEventArgs e)
+        {
+            (e.Node!.Content as FileManager)!.IsExpanded = e.Node.IsExpanded;
+        }
+
+        private void Bindable_NodeExpanded(object? sender, NodeExpandedCollapsedEventArgs e)
+        {
+            (e.Node!.Content as FileManager)!.IsExpanded = e.Node.IsExpanded;
+        }
+    }
+}
